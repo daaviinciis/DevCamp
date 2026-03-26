065ccf29f8df9237ac8a0912f41fb9d924a02d3b
daaviinciis <davimede.official@gmail.com>
2026-03-26 13:31:17 +0100

    ejercicios finalizados

---
+++ module2/checkpoint3/ejer8.py
@@ -0,0 +1,8 @@
+text = "Hola Mundo"
+num = 33
+lista = ["manzana", "banana", "cereza"]
+booleano = True
+
+nuevoNumero = num + 10
+
+print(f"Mi número original es {num} y el nuevo número es {nuevoNumero}");
---
+++ module2/checkpoint3/ejerExtra.py
@@ -0,0 +1,5 @@
+cadena = "Hola, ¿cómo estás? Lorea"
+posicion = cadena.find("Hola")
+
+newCadenaReemplazada = cadena.replace("Hola", "adiós")
+print(newCadenaReemplazada)
