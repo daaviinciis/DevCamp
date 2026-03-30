9284c065b8563ed891d040c6aab23dd31f0fbad1
daaviinciis <davimede.official@gmail.com>
2026-03-30 08:33:06 +0200

    ejercicio1 completo

--- module2/checkpoint4/checkpoint4.py
+++ module2/checkpoint4/checkpoint4.py
@@ -3,3 +3,13 @@ mi_lista = ["manzana", "banana", "cereza"]
 mi_tupla = ("rojo", "verde", "azul")
 mi_float = 5.7
 mi_entero = 10
+
+from decimal import Decimal
+mi_decimal = Decimal("3.14")
+
+mi_diccionario = {
+    "nombre": "Daavii",
+    "edad": 20,
+    "ciudad": "Donosti"
+}
+
