dd5c09c67e40651172c4fe1a034fc44abf18da64
daaviinciis <davimede.official@gmail.com>
2026-03-30 08:30:38 +0200

    ejercicio1 sin completar

---
+++ module2/checkpoint4/checkpoint4.py
@@ -0,0 +1,5 @@
+# Ejercicio 1:  Create a list, tuple, float, integer, decimal, and dictionary.
+mi_lista = ["manzana", "banana", "cereza"]
+mi_tupla = ("rojo", "verde", "azul")
+mi_float = 5.7
+mi_entero = 10
