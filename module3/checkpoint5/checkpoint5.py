37486c5dafb482e50a8773bcf69f9c587e9ff5b8
daaviinciis <davimede.official@gmail.com>
2026-03-31 08:49:07 +0200

    ejercicios checkpoint5 realizados

--- module3/checkpoint5/checkpoint5.py
+++ module3/checkpoint5/checkpoint5.py
@@ -7,3 +7,18 @@ def suma(a, b, c):
     return a + b + c
 
 print(suma(2, 3, 5))
+
+# Funcion lambda
+suma_lambda = lambda a, b, c: a + b + c
+
+print(suma_lambda(2, 3, 5))
+
+# Ejercicio lista
+nombre = 'Enrique'
+
+lista_nombre = ('Jessica', 'Paul', 'George', 'Henry', 'Adán')
+
+if nombre in lista_nombre:
+    print(nombre + " está en la lista")
+else:
+    print(nombre + " NO está en la lista")
