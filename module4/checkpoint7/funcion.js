2cea8cdf2603c4426e4202cb677af22c34943d3e
daaviinciis <davimede.official@gmail.com>
2026-04-08 13:15:00 +0200

    .md checkpoint 7 añadido

---
+++ module4/checkpoint7/funcion.js
@@ -0,0 +1,15 @@
+function calcular(a, b, c, d) {
+  let suma1 = a + b;
+  let suma2 = c + d;
+  let resultado = suma1 * suma2;
+
+  if (resultado > 50) {
+    console.log("El número es mayor que 50");
+  } else {
+    console.log("El número es menor que 50");
+  }
+
+  return resultado;
+}
+
+calcular(5,5,3,2);
